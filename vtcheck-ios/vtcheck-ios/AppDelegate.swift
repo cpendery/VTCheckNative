25b35ab4191fd28cb2f3244192ae2e6cd8bf455d
Sameer Dandekar <sameer@Sameers-MBP.fios-router.home>
2020-07-26 23:55:20 -0400

    add scan focus region image to app on startup

--- vtcheck-ios/vtcheck-ios/AppDelegate.swift
+++ vtcheck-ios/vtcheck-ios/AppDelegate.swift
@@ -14,7 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+       
+        // This function is given in ScanFocusRegion.swift
+        createScanFocusRegionImage()
         return true
     }
 
